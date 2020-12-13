eb447ee0120f2ed2972707fae01179c797b9deb3
Comforts Mulala <61155738+comforts@users.noreply.github.com>
2020-12-14 00:07:11 +0200

    Add files via upload

---
+++ resources/scripts/app.js
@@ -0,0 +1,5 @@
+document.getElementById('dropdown-menu').addEventListener('mouseenter', function(){
+    var dropdown = document.getElementById('app-dropdown');
+
+    dropdown.style.display = 'block';
+});
